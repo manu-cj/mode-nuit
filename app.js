fa23f72bc365f70592a864070363c448ab7a99ee
manucarenjot <carenjotje@gmail.com>
2021-11-25 13:31:13 +0100

    NightMode test

---
+++ app.js
@@ -0,0 +1,10 @@
+let window = document.getElementById('window');
+let body = document.querySelector('body');
+
+function nightMode() {
+    if (i>0){
+        window.style.backgroundColor = 'blue';
+        body.style.backgroundColor = 'black'
+    }
+}
+
